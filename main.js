563ad5c3fb53b4f0c46cd8d1229090e270217bec
ggorlen <gsgorlen@gmail.com>
2018-03-23 09:42:39 -0700

    add a working template to follow

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
-// Create the Phaser game object
-const game = new Phaser.Game(800, 600, Phaser.AUTO, "example");
+/**
+ * main.js 
+ *
+ * This is where global variables live 
+ * and the first state is started
+ */
 
-// Add game states
+// Global variables
+let text;
+const width = 800;
+const height = 600;
+
+// Create the Phaser game object
+const game = new Phaser.Game(width, height, Phaser.AUTO, "example");
+
+// Add states to the game
 game.state.add("menu", menuState);
 game.state.add("game", gameState);
 
